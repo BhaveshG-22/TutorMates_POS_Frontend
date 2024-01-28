3e08afc626a706fef49fb23b3026a655242278d5
BHAVESH GAVALI <bgavali@BHAVESHs-MacBook-Pro.local>
2024-01-27 22:20:51 -0500

    added code for frontend

---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        adjective: "#FFCC66", // Adjective
+        adverb: "#C69999", // Adverb
+        conjunction: "#CCCC99", // Conjunction
+        determiner: "#9999CC", // Determiner
+        noun: "#CCCCCC", // Noun
+        number: "#66CC99", // Number
+        preposition: "#FFCC99", // Preposition
+        pronoun: "#EE77EE", // Pronoun
+        verb: "#CCFF66", // Verb
+
+        coordconjuncn: "#CCCC99", // Conjunction
+         existentialthere: "#CCCCCC", // Noun
+        foreignword: "#CCCCCC", // Noun
+         adjectivecomparative: "#FFCC66", // Adjective
+        adjectivesuperlative: "#FFCC66", // Adjective
+        modal: "#CCFF66", // Verb
+        nounsingormass: "#CCC", // Noun
+        propernounsing: "#CCCCCC", // Noun
+        propernounplural: "#CCCCCC", // Noun
+        nounplural: "#CCCCCC", // Noun
+        possessiveending: "#CCCCCC", // Noun
+        predeterminer: "#9999CC", // Determiner
+         possessivepronoun: "#EE77EE", // Pronoun
+        personalpronoun: "#EE77EE", // Pronoun
+         adverbcomparative: "#C69999", // Adverb
+        adverbsuperlative: "#C69999", // Adverb
+        particle: "#CCCCCC", // Noun
+        symbol: "#CCCCCC", // Noun
+        interjection: "#CCCCCC", // Noun
+        verbbaseform: "#CCFF66", // Verb
+        verbpasttense: "#CCFF66", // Verb
+        verbgerund: "#CCFF66", // Verb
+        verbpastpart: "#CCFF66", // Verb
+        verbpresent: "#CCFF66", // Verb
+        verbpresent: "#CCFF66", // Verb
+        whdeterminer: "#9999CC", // Determiner
+        whpronoun: "#EE77EE", // Pronoun
+        possessivewh: "#EE77EE", // Pronoun
+        whadverb: "#C69999", // Adverb
+      },
+    },
+  },
+  plugins: [],
+};
